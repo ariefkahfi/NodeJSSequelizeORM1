381039e7b9855af0d9ef091d0aab4796158ed1de
ariefkahfi <ariefkahfi@gmail.com>
2018-03-12 19:57:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("s_orm1","arief","123",{
+    dialect:"mysql"
+});
+
+const PersonModel = sequelize.define("person",{
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        field:"person_id"
+    },
+    name:{
+        type:Sequelize.STRING
+    }
+},{
+    timestamps:false,
+    tableName:"person"
+});
+
+// create a tables Model.sync()
+// PersonModel.sync()
+//     .then((val)=>{
+//         console.log(val);
+//     },(err)=>{
+//         console.log(err);
+//     })
+
+// create new tables , if exists drop it Model.synct({force : true})
+// PersonModel.sync({force:true})
+//     .then((val)=>{
+//         console.log(val);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     });
+
+// create a model instance then save into database
+// PersonModel.create({name:"aa1"})
+// .then((val)=>{
+//     console.log(val);
+// },(err)=>{
+//     console.log(err);
+// });
